46a45523ebfa988810900f76a67247e202417e8e
bodpunk <bodpunk@mail.ru>
2017-09-01 23:04:38 +0500

    Added images and carousel with plugin OwlCarousel

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+  $(".owl-carousel").owlCarousel( {
+    items: 1,
+    margin: 10,
+    slideBy: 1,
+    loop: true,
+    navText: ["",""],
+    autoplay: true
+  });
+});
